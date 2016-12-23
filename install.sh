a134749cdebc81f4c17881c66cc87ea38d1e2a94
AmirSbss <mrahs@emeil.ir>
2016-12-23 19:23:18 +0330

    V 2.7

--- install.sh
+++ install.sh
@@ -3,16 +3,17 @@ wget "https://valtman.name/files/telegram-cli-1222"
 sudo apt-get install ppa-purge
 sudo apt-get install python-pip
 sudo pip instal redis
+sudo servoce redis-server restart
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo ppa-purge
-chmod 777 create.py
-chmod 777 run.py
+chmod 777 creator.py
+chmod 777 run.sh
 chmod 777 telegram-cli-1222
 RED='\033[0;31m'
 NC='\033[0m'
 CYAN='\033[0;36m'
-echo -e "${CYAN}Installation Completed! Create a bot with ./create.py ${NC}"
+echo -e "${CYAN}Installation Completed! Create a bot with ./creator.py ${NC}"
 exit
