5522baf77574856aece60fd39d5cd207eb72d82a
AmirSbss <mrahs@emeil.ir>
2016-12-23 12:22:31 +0330

    Update redis.lua

--- redis.lua
+++ redis.lua
@@ -1,5 +1,5 @@
-local Redis = (loadfile "./libs/lua-redis.lua")()
-local FakeRedis = (loadfile "./libs/fakeredis.lua")()
+local Redis = (loadfile "lua-redis.lua")()
+local FakeRedis = (loadfile "fakeredis.lua")()
 
 local params = {
   host = '127.0.0.1',
