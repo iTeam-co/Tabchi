abcb0427776011bc7e78a6ce4158a31fe61f710d
Mrhalix <aminaleahmad@yahoo.com>
2016-12-23 16:00:25 +0330

    Update tabchi.sh

--- tabchi.sh
+++ tabchi.sh
@@ -12,6 +12,7 @@ install() {
   sudo apt-get dist-upgrade
   sudo ppa-purge
   sudo apt-get install gcc-6
+  sudo service redis-server start
   chmod 777 telegram-cli-1222
   RED='\033[0;31m'
   NC='\033[0m'
