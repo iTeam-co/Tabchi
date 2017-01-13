0411b6c19fa2ca28e537b55e2feca1e6fec5da28
AmirSbss <mrahs@emeil.ir>
2017-01-13 14:47:31 +0330

    Update base.lua

--- base.lua
+++ base.lua
@@ -23,6 +23,7 @@ function tdcli_update_callback(data)
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Bot Reloaded*", 1, "md")
   elseif data.message_ and data.message_.content_.text_ and data.message_.content_.text_ == "/gitpull" and data.message_.sender_user_id_ == tonumber(redis:get("tabchi:" .. tabchi_id ..":fullsudo")) then
     io.popen("git fetch --all && git reset --hard origin/master && git pull origin master"):read("*all")
+    reload()
     tdcli.sendMessage(data.message_.chat_id_, 0, 1, "*Updates Received And Bot Reloaded*", 1, "md")
   end
 end
