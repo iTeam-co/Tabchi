26e1f55c5ae84a75f50d60f49a00d9123c6da361
AmirSbss <mrahs@emeil.ir>
2017-01-25 22:32:51 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 wget "https://valtman.name/files/telegram-cli-1222"
-sudo apt-get install libreadline-dev
-sudo apt install libreadline-dev 
-sudo apt-get -y install libreadline6 libreadline6-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties gcc-6
+sudo apt-get -y install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties gcc-6
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo ppa-purge
 sudo service redis-server restart
-sudo apt-get install python-pip
-sudo pip install redis
 chmod 777 telegram-cli-1222
 RED='\033[0;31m'
 NC='\033[0m'
