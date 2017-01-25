eb2a31db278c0735e2cbd4e7e0c5fe14bd5d3470
AmirSbss <mrahs@emeil.ir>
2017-01-25 22:38:12 +0330

    Create updater.lua

---
+++ updater.lua
@@ -0,0 +1,7 @@
+local pfile = io.popen('ls')
+for filename in pfile:lines() do
+  if filename:match('tabchi%-(%d+)%.lua') then
+    local text, ok = io.open(filename,'r'):read('*a'):gsub([[localfile("tabchi.lua")()]],[[tabchi = dofile("tabchi.lua")]]):gsub([[update(data, tabchi_id)]],[[tabchi.update(data, tabchi_id)]])
+    io.open(filename,'w'):write(text):close()
+  end
+end
