c57d3c550f7c741f3d74fd7f2b2775f24482f7f5
AmirSbss <mrahs@emeil.ir>
2017-01-26 17:37:26 +0330

    TesT Version
    Anti crash/Cache cleanup for running all bots using tmux

---
+++ anticrash.sh
@@ -0,0 +1,18 @@
+while true ; do
+  killall tmux
+  for entr in tabchi-*.sh ; do
+    entry="${entr/.sh/}"
+    rm -rf ~/.telegram-cli/$entry/data/animation/*
+    rm -rf ~/.telegram-cli/$entry/data/audio/*
+    rm -rf ~/.telegram-cli/$entry/data/document/*
+    rm -rf ~/.telegram-cli/$entry/data/photo/*
+    rm -rf ~/.telegram-cli/$entry/data/sticker/*
+    rm -rf ~/.telegram-cli/$entry/data/temp/*
+    rm -rf ~/.telegram-cli/$entry/data/video/*
+    rm -rf ~/.telegram-cli/$entry/data/voice/*
+    rm -rf ~/.telegram-cli/$entry/data/profile_photo/*
+    tmux new-session -d -s $entry "./$entr"
+    tmux detach -s $entry
+  done
+  sleep 1800
+done
