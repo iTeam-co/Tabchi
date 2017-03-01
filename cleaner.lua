4f6b983a152c6bddc2fbd9fcf3b2b1058c2f2c89
AmirSbss <mrahs@emeil.ir>
2017-03-01 21:21:12 +0330

    Create cleaner.lua

---
+++ cleaner.lua
@@ -0,0 +1,6 @@
+redis = (loadfile "redis.lua")()
+io.write("Enter Tabchi ID : ")
+local last = io.read()
+io.popen('rm -rf ~/.telegram-cli/tabchi-'..last..' tabchi-'..last..'.lua tabchi-'..last..'.sh tabchi_'..last..'_logs.txt')
+redis:del('tabchi:'..last..':*')
+print("Done!\nAll Data/Files Of Tabchi Deleted\nTabchi ID : "..last)
