01df54ff4dec8a50286e5da52acdc9a0ea8c77fa
AmirSbss <mrahs@emeil.ir>
2017-03-12 20:28:07 +0330

    Proxy for bots
    Set chmod to 777 then run and enjoy script (just a time)

---
+++ torinstall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install -y tor
+sudo service tor start
+sudo export http_proxy=socks5://127.0.0.1:9050 https_proxy=socks5://127.0.0.1:9050
+NC='\033[0m'
+CYAN='\033[0;36m'
+echo -e "${CYAN}All Done! Tor Installed And Applied..., Enjoy Your Bots${NC}"
